b0561d4825af4a973f8ebe04807747beb933f0da
bychv <swgloi@163.com>
2025-02-05 22:34:18 +0800

    Site updated: 2025-02-05 22:34:18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1738754112532";const preCache = ["/images/taichi.png","/images/banner16.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1738766057443";const preCache = ["/images/taichi.png","/images/banner16.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
